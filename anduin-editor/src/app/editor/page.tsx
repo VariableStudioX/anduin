56aabba6b3c38fe841cf005122e756eb2262b748
icepy <xiangwenwe@gmail.com>
2023-08-15 23:30:38 +0800

    chore: 调整页面布局

--- anduin-editor/src/app/editor/page.tsx
+++ anduin-editor/src/app/editor/page.tsx
@@ -6,20 +6,21 @@ import Navs from '@/navs';
 
 export default function Editor(){
   return (
-    <div className="w-full h-screen flex flex-row flex-nowrap">
-      <div className="w-60">
+    <main className="w-full bg-white flex flex-row flex-nowrap h-screen">
+      <header className="w-full fixed h-10 border-b border-gray-200 bg-white">Actions</header>
+      <div className="w-60 border-r border-gray-200 pt-10">
         <div className="p-2.5">
           <Navs/>
         </div>
       </div>
-      <div className="flex-1 w-full">
+      <div className="flex-1 w-full pt-10">
         <Simulator target="simple-750"/>
       </div>
-      <div className="w-80">
+      <div className="w-96 border-l border-gray-200 pt-10">
         <div className="p-2.5">
           <Forms />
         </div>
       </div>
-    </div>
+    </main>
   )
 }
